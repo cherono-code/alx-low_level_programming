7fba19dc0b01daf05e3fa9849736017a223342ec
cherono-code <cheronodaisy38@gmail.com>
2023-02-20 10:59:34 -0800

    lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:'printing lowercase alphabets'
+ * Return: always 0
+ */
+
+int main(void)
+{
+int i = 'a';
+	for (i = 'a'; i <= 'z'; i++)
+		putchar (i : \n);
+	return (0);
+}
