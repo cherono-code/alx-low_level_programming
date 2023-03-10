9af52cb5154a5e34098b1eadd240e9b37da08444
cherono-code <cheronodaisy38@gmail.com>
2023-03-10 11:25:11 -0800

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Description: 'print number of arguments passed'
+ * @argc: number of command line arguments
+ * @argv: vector of c strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+
+{
+(void) argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
