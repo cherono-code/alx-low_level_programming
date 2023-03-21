bf33fb7fe5f06a714523936e9f1348a4d8d348a8
cherono-code <cheronodaisy38@gmail.com>
2023-03-21 12:27:11 -0700

    name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * Description: 'print the file it was compiled from'
+ * Return: (0) Always
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
