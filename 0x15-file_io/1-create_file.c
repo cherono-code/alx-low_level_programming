c2f0572f8de21500a989447f270f7a922d028456
cherono-code <cheronodaisy38@gmail.com>
2023-04-19 23:43:34 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(file, text_content, len);
 
 	if (file == -1 || wr == -1)
