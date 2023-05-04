81a106179bce432035fe2fbab698f414f8abd338
cherono-code <cheronodaisy38@gmail.com>
2023-05-04 07:06:24 -0700

    printing binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -25,27 +25,3 @@ void print_binary(unsigned long int n)
 	if (!count)
 		_putchar('0');
 }
-
-2 - get_bit.c
-
-#include "main.h"
-
-/**
- * get_bit - returns the value of a bit at a given index
- * @n: number to be searched
- * @index: index of the bit
- *
- * Return: value of the bit
- */
-
-int get_bit(unsigned long int n, unsigned int index)
-{
-	int bitvalue;
-
-	if (index > 63)
-		return (-1);
-
-	bitvalue = (n >> index) & 1;
-
-	return (bitvalue);
-}
