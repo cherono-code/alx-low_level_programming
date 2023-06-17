2a33de1dbfcfb75a183d16d190c72894eda0589a
cherono-code <cheronodaisy38@gmail.com>
2023-06-17 10:19:07 -0700

    cor 3

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 
 /**
- * free_dlistint - frees a doubly linked list
- * @head: pointer to the list to free
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: head of the list
+ * Return: no return
  */
 void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *temp;
+	dlistint_t *tmp;
 
-	while (head)
+	if (head != NULL)
+		while (head->prev != NULL)
+			head = head->prev;
+
+	while ((tmp = head) != NULL)
 	{
-		temp = head->nxt;
-		free(head);
-		head = temp;
+		head = head->next;
+		free(tmp);
 	}
 }
